d71cf5894589089d15e5bc76d527ac9bdd4fb3d7
Rahma405 <mechri.rahma@esprit.tn>
2025-10-04 14:55:46 +0100

    Ajout du module gestion des clients Qt

---
+++ build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_rahma_mechri_FILE C:/Users/ProBook/OneDrive - ESPRIT/Documents/rahma_mechri/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/rahma_mechri.exe)
+set(__QT_DEPLOY_TARGET_rahma_mechri_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_rahma_mechri_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6PrintSupport.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
