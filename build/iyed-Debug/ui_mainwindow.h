ce78c2224b31c3d891e2589d7ef3ea4963b9c960
Iyed222 <iyedchokri40@gmail.com>
2025-10-05 17:34:23 +0200

    second push

--- build/iyed-Debug/ui_mainwindow.h
+++ build/iyed-Debug/ui_mainwindow.h
@@ -60,6 +60,14 @@ public:
     QLineEdit *lineEdit_9;
     QPushButton *pushButton_7;
     QFrame *line;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_8;
+    QPushButton *pushButton_9;
+    QPushButton *pushButton_10;
+    QPushButton *pushButton_11;
+    QPushButton *pushButton_12;
+    QPushButton *pushButton_13;
+    QFrame *line_3;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -67,12 +75,12 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(1388, 607);
+        MainWindow->resize(1390, 625);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
         label = new QLabel(centralwidget);
         label->setObjectName("label");
-        label->setGeometry(QRect(560, 10, 191, 31));
+        label->setGeometry(QRect(710, 20, 191, 31));
         label->setStyleSheet(QString::fromUtf8("QLabel {\n"
 "    color: #8A3B0B;           /* Texte brun fonc\303\251 */\n"
 "    border-radius: 8px;\n"
@@ -83,7 +91,7 @@ public:
 ""));
         label_2 = new QLabel(centralwidget);
         label_2->setObjectName("label_2");
-        label_2->setGeometry(QRect(70, 70, 171, 31));
+        label_2->setGeometry(QRect(220, 80, 171, 31));
         label_2->setStyleSheet(QString::fromUtf8("QLabel {\n"
 "    background-color: #8A3B0B;  /* Couleur de fond */\n"
 "    color: white;               /* Texte */\n"
@@ -94,67 +102,31 @@ public:
 ""));
         label_3 = new QLabel(centralwidget);
         label_3->setObjectName("label_3");
-        label_3->setGeometry(QRect(0, 160, 131, 31));
-        label_3->setStyleSheet(QString::fromUtf8("QLabel {\n"
-"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
-"    border-radius: 8px;\n"
-"    padding: 5px 10px;\n"
-"    font-weight: bold;\n"
-"}\n"
-""));
+        label_3->setGeometry(QRect(160, 170, 131, 31));
+        label_3->setStyleSheet(QString::fromUtf8("color: rgb(0, 85, 255);"));
         label_4 = new QLabel(centralwidget);
         label_4->setObjectName("label_4");
-        label_4->setGeometry(QRect(0, 210, 151, 31));
-        label_4->setStyleSheet(QString::fromUtf8("QLabel {\n"
-"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
-"    border-radius: 8px;\n"
-"    padding: 5px 10px;\n"
-"    font-weight: bold;\n"
-"}\n"
-""));
+        label_4->setGeometry(QRect(160, 220, 151, 31));
+        label_4->setStyleSheet(QString::fromUtf8("color: rgb(0, 85, 255);"));
         label_5 = new QLabel(centralwidget);
         label_5->setObjectName("label_5");
-        label_5->setGeometry(QRect(10, 370, 63, 31));
-        label_5->setStyleSheet(QString::fromUtf8("QLabel {\n"
-"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
-"    border-radius: 8px;\n"
-"    padding: 5px 10px;\n"
-"    font-weight: bold;\n"
-"}\n"
-""));
+        label_5->setGeometry(QRect(160, 380, 63, 31));
+        label_5->setStyleSheet(QString::fromUtf8("color: rgb(0, 85, 255);"));
         label_6 = new QLabel(centralwidget);
         label_6->setObjectName("label_6");
-        label_6->setGeometry(QRect(0, 320, 111, 31));
-        label_6->setStyleSheet(QString::fromUtf8("QLabel {\n"
-"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
-"    border-radius: 8px;\n"
-"    padding: 5px 10px;\n"
-"    font-weight: bold;\n"
-"}\n"
-""));
+        label_6->setGeometry(QRect(160, 330, 111, 31));
+        label_6->setStyleSheet(QString::fromUtf8("color: rgb(0, 85, 255);"));
         label_7 = new QLabel(centralwidget);
         label_7->setObjectName("label_7");
-        label_7->setGeometry(QRect(10, 270, 63, 31));
-        label_7->setStyleSheet(QString::fromUtf8("QLabel {\n"
-"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
-"    border-radius: 8px;\n"
-"    padding: 5px 10px;\n"
-"    font-weight: bold;\n"
-"}\n"
-""));
+        label_7->setGeometry(QRect(160, 280, 63, 31));
+        label_7->setStyleSheet(QString::fromUtf8("color: rgb(0, 85, 255);"));
         label_8 = new QLabel(centralwidget);
         label_8->setObjectName("label_8");
-        label_8->setGeometry(QRect(0, 470, 101, 31));
-        label_8->setStyleSheet(QString::fromUtf8("QLabel {\n"
-"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
-"    border-radius: 8px;\n"
-"    padding: 5px 10px;\n"
-"    font-weight: bold;\n"
-"}\n"
-""));
+        label_8->setGeometry(QRect(160, 480, 121, 31));
+        label_8->setStyleSheet(QString::fromUtf8("color: rgb(0, 85, 255);"));
         label_9 = new QLabel(centralwidget);
         label_9->setObjectName("label_9");
-        label_9->setGeometry(QRect(690, 70, 171, 31));
+        label_9->setGeometry(QRect(840, 80, 171, 31));
         label_9->setStyleSheet(QString::fromUtf8("QLabel {\n"
 "    background-color: #8A3B0B;  /* Couleur de fond */\n"
 "    color: white;               /* Texte */\n"
@@ -166,7 +138,7 @@ public:
 ""));
         pushButton = new QPushButton(centralwidget);
         pushButton->setObjectName("pushButton");
-        pushButton->setGeometry(QRect(290, 270, 93, 29));
+        pushButton->setGeometry(QRect(440, 280, 93, 29));
         pushButton->setStyleSheet(QString::fromUtf8("QPushButton {\n"
 "    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
 "    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
@@ -185,7 +157,7 @@ public:
 ""));
         pushButton_2 = new QPushButton(centralwidget);
         pushButton_2->setObjectName("pushButton_2");
-        pushButton_2->setGeometry(QRect(290, 320, 93, 29));
+        pushButton_2->setGeometry(QRect(440, 330, 93, 29));
         pushButton_2->setStyleSheet(QString::fromUtf8("QPushButton {\n"
 "    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
 "    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
@@ -204,10 +176,10 @@ public:
 ""));
         lineEdit = new QLineEdit(centralwidget);
         lineEdit->setObjectName("lineEdit");
-        lineEdit->setGeometry(QRect(480, 180, 141, 21));
+        lineEdit->setGeometry(QRect(630, 190, 141, 21));
         pushButton_4 = new QPushButton(centralwidget);
         pushButton_4->setObjectName("pushButton_4");
-        pushButton_4->setGeometry(QRect(620, 180, 111, 31));
+        pushButton_4->setGeometry(QRect(770, 190, 111, 31));
         pushButton_4->setStyleSheet(QString::fromUtf8("QPushButton {\n"
 "    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
 "    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
@@ -226,30 +198,30 @@ public:
 ""));
         lineEdit_2 = new QLineEdit(centralwidget);
         lineEdit_2->setObjectName("lineEdit_2");
-        lineEdit_2->setGeometry(QRect(160, 160, 113, 26));
+        lineEdit_2->setGeometry(QRect(310, 170, 113, 26));
         lineEdit_3 = new QLineEdit(centralwidget);
         lineEdit_3->setObjectName("lineEdit_3");
-        lineEdit_3->setGeometry(QRect(160, 210, 113, 26));
+        lineEdit_3->setGeometry(QRect(310, 220, 113, 26));
         lineEdit_4 = new QLineEdit(centralwidget);
         lineEdit_4->setObjectName("lineEdit_4");
-        lineEdit_4->setGeometry(QRect(160, 270, 113, 26));
+        lineEdit_4->setGeometry(QRect(310, 280, 113, 26));
         lineEdit_5 = new QLineEdit(centralwidget);
         lineEdit_5->setObjectName("lineEdit_5");
-        lineEdit_5->setGeometry(QRect(160, 320, 113, 26));
+        lineEdit_5->setGeometry(QRect(310, 330, 113, 26));
         lineEdit_6 = new QLineEdit(centralwidget);
         lineEdit_6->setObjectName("lineEdit_6");
-        lineEdit_6->setGeometry(QRect(160, 360, 113, 26));
+        lineEdit_6->setGeometry(QRect(310, 380, 113, 26));
         lineEdit_7 = new QLineEdit(centralwidget);
         lineEdit_7->setObjectName("lineEdit_7");
-        lineEdit_7->setGeometry(QRect(160, 410, 113, 26));
+        lineEdit_7->setGeometry(QRect(310, 430, 113, 26));
         line_2 = new QFrame(centralwidget);
         line_2->setObjectName("line_2");
-        line_2->setGeometry(QRect(390, 130, 20, 351));
+        line_2->setGeometry(QRect(540, 140, 20, 351));
         line_2->setFrameShape(QFrame::Shape::VLine);
         line_2->setFrameShadow(QFrame::Shadow::Sunken);
         label_10 = new QLabel(centralwidget);
         label_10->setObjectName("label_10");
-        label_10->setGeometry(QRect(780, 130, 91, 31));
+        label_10->setGeometry(QRect(930, 140, 91, 31));
         label_10->setStyleSheet(QString::fromUtf8("QLabel {\n"
 "    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
 "    border-radius: 8px;\n"
@@ -262,7 +234,7 @@ public:
         comboBox->addItem(QString());
         comboBox->addItem(QString());
         comboBox->setObjectName("comboBox");
-        comboBox->setGeometry(QRect(870, 130, 91, 31));
+        comboBox->setGeometry(QRect(1020, 140, 91, 31));
         comboBox->setStyleSheet(QString::fromUtf8("QComboBox {\n"
 "    background-color: #E59D25;   /* Fond citrine dor\303\251 */\n"
 "    color: #2E4365;               /* Texte bleu fonc\303\251 */\n"
@@ -327,7 +299,7 @@ public:
         QTableWidgetItem *__qtablewidgetitem13 = new QTableWidgetItem();
         tableWidget->setVerticalHeaderItem(6, __qtablewidgetitem13);
         tableWidget->setObjectName("tableWidget");
-        tableWidget->setGeometry(QRect(410, 260, 761, 191));
+        tableWidget->setGeometry(QRect(560, 270, 761, 191));
         tableWidget->setStyleSheet(QString::fromUtf8("QTabBar::tab {\n"
 "    background: #8A3B0B;  /* brun fonc\303\251 */\n"
 "    color: white;\n"
@@ -353,7 +325,7 @@ public:
 ""));
         pushButton_5 = new QPushButton(centralwidget);
         pushButton_5->setObjectName("pushButton_5");
-        pushButton_5->setGeometry(QRect(1100, 460, 111, 29));
+        pushButton_5->setGeometry(QRect(1250, 470, 111, 29));
         pushButton_5->setStyleSheet(QString::fromUtf8("QPushButton {\n"
 "    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
 "    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
@@ -373,7 +345,7 @@ public:
 ""));
         pushButton_6 = new QPushButton(centralwidget);
         pushButton_6->setObjectName("pushButton_6");
-        pushButton_6->setGeometry(QRect(800, 460, 131, 29));
+        pushButton_6->setGeometry(QRect(1030, 470, 131, 29));
         pushButton_6->setStyleSheet(QString::fromUtf8("QPushButton {\n"
 "    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
 "    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
@@ -392,33 +364,21 @@ public:
 ""));
         label_11 = new QLabel(centralwidget);
         label_11->setObjectName("label_11");
-        label_11->setGeometry(QRect(10, 110, 63, 31));
-        label_11->setStyleSheet(QString::fromUtf8("QLabel {\n"
-"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
-"    border-radius: 8px;\n"
-"    padding: 5px 10px;\n"
-"    font-weight: bold;\n"
-"}\n"
-""));
+        label_11->setGeometry(QRect(160, 120, 63, 31));
+        label_11->setStyleSheet(QString::fromUtf8("color: rgb(0, 85, 255);"));
         lineEdit_8 = new QLineEdit(centralwidget);
         lineEdit_8->setObjectName("lineEdit_8");
-        lineEdit_8->setGeometry(QRect(160, 110, 113, 26));
+        lineEdit_8->setGeometry(QRect(310, 120, 113, 26));
         label_12 = new QLabel(centralwidget);
         label_12->setObjectName("label_12");
-        label_12->setGeometry(QRect(10, 420, 63, 31));
-        label_12->setStyleSheet(QString::fromUtf8("QLabel {\n"
-"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
-"    border-radius: 8px;\n"
-"    padding: 5px 10px;\n"
-"    font-weight: bold;\n"
-"}\n"
-""));
+        label_12->setGeometry(QRect(160, 430, 63, 31));
+        label_12->setStyleSheet(QString::fromUtf8("color: rgb(0, 85, 255);"));
         lineEdit_9 = new QLineEdit(centralwidget);
         lineEdit_9->setObjectName("lineEdit_9");
-        lineEdit_9->setGeometry(QRect(160, 470, 113, 26));
+        lineEdit_9->setGeometry(QRect(310, 480, 113, 26));
         pushButton_7 = new QPushButton(centralwidget);
         pushButton_7->setObjectName("pushButton_7");
-        pushButton_7->setGeometry(QRect(460, 480, 93, 29));
+        pushButton_7->setGeometry(QRect(852, 470, 111, 29));
         pushButton_7->setStyleSheet(QString::fromUtf8("QPushButton {\n"
 "    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
 "    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
@@ -437,13 +397,151 @@ public:
 ""));
         line = new QFrame(centralwidget);
         line->setObjectName("line");
-        line->setGeometry(QRect(40, 50, 1191, 20));
+        line->setGeometry(QRect(20, 60, 1361, 20));
         line->setFrameShape(QFrame::Shape::HLine);
         line->setFrameShadow(QFrame::Shadow::Sunken);
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName("pushButton_3");
+        pushButton_3->setGeometry(QRect(20, 80, 121, 41));
+        pushButton_3->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
+"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
+"    border-radius: 8px;\n"
+"    padding: 5px 15px;\n"
+"    font-weight: bold;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: #D48A1F; /* Plus fonc\303\251 au survol */\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: #B37319; /* Encore plus fonc\303\251 quand press\303\251 */\n"
+"}\n"
+""));
+        pushButton_8 = new QPushButton(centralwidget);
+        pushButton_8->setObjectName("pushButton_8");
+        pushButton_8->setGeometry(QRect(20, 170, 111, 29));
+        pushButton_8->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
+"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
+"    border-radius: 8px;\n"
+"    padding: 5px 15px;\n"
+"    font-weight: bold;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: #D48A1F; /* Plus fonc\303\251 au survol */\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: #B37319; /* Encore plus fonc\303\251 quand press\303\251 */\n"
+"}\n"
+""));
+        pushButton_9 = new QPushButton(centralwidget);
+        pushButton_9->setObjectName("pushButton_9");
+        pushButton_9->setGeometry(QRect(20, 230, 111, 31));
+        pushButton_9->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
+"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
+"    border-radius: 8px;\n"
+"    padding: 5px 15px;\n"
+"    font-weight: bold;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: #D48A1F; /* Plus fonc\303\251 au survol */\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: #B37319; /* Encore plus fonc\303\251 quand press\303\251 */\n"
+"}\n"
+""));
+        pushButton_10 = new QPushButton(centralwidget);
+        pushButton_10->setObjectName("pushButton_10");
+        pushButton_10->setGeometry(QRect(20, 290, 111, 31));
+        pushButton_10->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
+"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
+"    border-radius: 8px;\n"
+"    padding: 5px 15px;\n"
+"    font-weight: bold;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: #D48A1F; /* Plus fonc\303\251 au survol */\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: #B37319; /* Encore plus fonc\303\251 quand press\303\251 */\n"
+"}\n"
+""));
+        pushButton_11 = new QPushButton(centralwidget);
+        pushButton_11->setObjectName("pushButton_11");
+        pushButton_11->setGeometry(QRect(10, 350, 141, 41));
+        pushButton_11->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
+"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
+"    border-radius: 8px;\n"
+"    padding: 5px 15px;\n"
+"    font-weight: bold;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: #D48A1F; /* Plus fonc\303\251 au survol */\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: #B37319; /* Encore plus fonc\303\251 quand press\303\251 */\n"
+"}\n"
+""));
+        pushButton_12 = new QPushButton(centralwidget);
+        pushButton_12->setObjectName("pushButton_12");
+        pushButton_12->setGeometry(QRect(20, 410, 121, 31));
+        pushButton_12->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
+"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
+"    border-radius: 8px;\n"
+"    padding: 5px 15px;\n"
+"    font-weight: bold;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: #D48A1F; /* Plus fonc\303\251 au survol */\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: #B37319; /* Encore plus fonc\303\251 quand press\303\251 */\n"
+"}\n"
+""));
+        pushButton_13 = new QPushButton(centralwidget);
+        pushButton_13->setObjectName("pushButton_13");
+        pushButton_13->setGeometry(QRect(20, 470, 121, 29));
+        pushButton_13->setStyleSheet(QString::fromUtf8("QPushButton {\n"
+"    background-color: #E59D25; /* Couleur principale citrine dor\303\251 */\n"
+"    color: #2E4365;             /* Texte bleu fonc\303\251 */\n"
+"    border-radius: 8px;\n"
+"    padding: 5px 15px;\n"
+"    font-weight: bold;\n"
+"}\n"
+"\n"
+"QPushButton:hover {\n"
+"    background-color: #D48A1F; /* Plus fonc\303\251 au survol */\n"
+"}\n"
+"\n"
+"QPushButton:pressed {\n"
+"    background-color: #B37319; /* Encore plus fonc\303\251 quand press\303\251 */\n"
+"}\n"
+""));
+        line_3 = new QFrame(centralwidget);
+        line_3->setObjectName("line_3");
+        line_3->setGeometry(QRect(150, 90, 20, 471));
+        line_3->setFrameShape(QFrame::Shape::VLine);
+        line_3->setFrameShadow(QFrame::Shadow::Sunken);
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 1388, 26));
+        menubar->setGeometry(QRect(0, 0, 1390, 26));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
@@ -496,6 +594,13 @@ public:
         label_11->setText(QCoreApplication::translate("MainWindow", "Id:", nullptr));
         label_12->setText(QCoreApplication::translate("MainWindow", "Mail:", nullptr));
         pushButton_7->setText(QCoreApplication::translate("MainWindow", "stat", nullptr));
+        pushButton_3->setText(QCoreApplication::translate("MainWindow", "Acceuil", nullptr));
+        pushButton_8->setText(QCoreApplication::translate("MainWindow", "employ\303\251", nullptr));
+        pushButton_9->setText(QCoreApplication::translate("MainWindow", "produits", nullptr));
+        pushButton_10->setText(QCoreApplication::translate("MainWindow", "RDV", nullptr));
+        pushButton_11->setText(QCoreApplication::translate("MainWindow", "commande facture", nullptr));
+        pushButton_12->setText(QCoreApplication::translate("MainWindow", "fournisseurs", nullptr));
+        pushButton_13->setText(QCoreApplication::translate("MainWindow", "clients", nullptr));
     } // retranslateUi
 
 };
